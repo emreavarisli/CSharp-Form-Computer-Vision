1dc7bead6078849e68908ae70c80096bf7c01059
Hasan Emre AVARİSLİ <emreavarisli@gmail.com>
2024-03-26 01:50:49 +0300

    Add project files.

---
+++ computer-vision/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace computer_vision
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea5 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend5 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea6 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend6 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(253, 238);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(639, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(253, 238);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // chart1
+            // 
+            chartArea5.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea5);
+            legend5.Name = "Legend1";
+            this.chart1.Legends.Add(legend5);
+            this.chart1.Location = new System.Drawing.Point(12, 256);
+            this.chart1.Name = "chart1";
+            series5.ChartArea = "ChartArea1";
+            series5.Legend = "Legend1";
+            series5.Name = "Series1";
+            this.chart1.Series.Add(series5);
+            this.chart1.Size = new System.Drawing.Size(407, 182);
+            this.chart1.TabIndex = 2;
+            this.chart1.Text = "chart1";
+            // 
+            // chart2
+            // 
+            chartArea6.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea6);
+            legend6.Name = "Legend1";
+            this.chart2.Legends.Add(legend6);
+            this.chart2.Location = new System.Drawing.Point(485, 256);
+            this.chart2.Name = "chart2";
+            series6.ChartArea = "ChartArea1";
+            series6.Legend = "Legend1";
+            series6.Name = "Series1";
+            this.chart2.Series.Add(series6);
+            this.chart2.Size = new System.Drawing.Size(407, 182);
+            this.chart2.TabIndex = 3;
+            this.chart2.Text = "chart2";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(366, 88);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 37);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Yükle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(459, 88);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(87, 37);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "İşlem";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(366, 61);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(180, 21);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(366, 131);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(180, 21);
+            this.comboBox2.TabIndex = 7;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(907, 450);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+    }
+}
+
---
+++ computer-vision/Form1.cs
@@ -0,0 +1,419 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace computer_vision
+{
+    public partial class Form1 : Form
+    {
+        private int secilenCombobox;
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.Items.Add("Gri Formata Çevir");
+            comboBox1.Items.Add("Görüntünün Histogramını Alma");
+            comboBox1.Items.Add("K-Means");
+            comboBox1.Items.Add("K-Means Görüntüsünün Histogramını Alma");
+            comboBox1.Items.Add("Binary Formata Çevir");
+            comboBox1.Items.Add("Görüntünün Sobel Kenar Bulma");
+
+            comboBox1.SelectedIndex = 0;
+
+        }
+
+        int esik;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Resim Yükleme Butonu
+            OpenFileDialog yukle = new OpenFileDialog();
+            yukle.ShowDialog();
+
+            pictureBox1.ImageLocation = yukle.FileName;
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //İşlem Butonu
+            string secilenSecenek = comboBox1.SelectedItem.ToString();
+
+            switch (secilenSecenek)
+            {
+                case "Gri Formata Çevir":
+
+                    try
+                    {
+                        Bitmap image = new Bitmap(pictureBox1.Image);
+                        Bitmap gri = griYap(image);
+                        pictureBox2.Image = gri;
+                        pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Fotoğraf Yükleyiniz!");
+                    }
+
+                    break;
+
+                case "Görüntünün Histogramını Alma":
+
+                    Bitmap bmp = new Bitmap(pictureBox2.Image);
+
+                    int[] histogramG = new int[256];
+
+                    for (int y = 0; y < bmp.Height; y++)
+                    {
+                        for (int x = 0; x < bmp.Width; x++)
+                        {
+                            Color color = bmp.GetPixel(x, y);
+                            histogramG[color.G]++;
+                        }
+                    }
+
+                    chart1.Series.Clear();
+
+                    Series seriesG = new Series("Green");
+                    seriesG.Color = Color.Green;
+
+                    for (int i = 0; i < 256; i++)
+                    {
+                        seriesG.Points.AddXY(i, histogramG[i]);
+                    }
+
+                    chart1.Series.Add(seriesG);
+                    chart1.Visible = true;
+
+                    break;
+
+                case "K-Means":
+
+                    pictureBox2.Image = KMeansSegmentasyon(pictureBox2.Image, secilenCombobox);
+
+
+                    break;
+                case "K-Means Görüntüsünün Histogramını Alma":
+
+                    Bitmap kmeansbmp = new Bitmap(pictureBox2.Image);
+
+                    int[] histogramB = new int[256];
+
+                    for (int y = 0; y < kmeansbmp.Height; y++)
+                    {
+                        for (int x = 0; x < kmeansbmp.Width; x++)
+                        {
+                            Color color = kmeansbmp.GetPixel(x, y);
+                            histogramB[color.G]++;
+                        }
+                    }
+
+                    chart2.Series.Clear();
+
+                    Series seriesB = new Series("Blue");
+                    seriesB.Color = Color.Blue;
+
+                    for (int i = 0; i < 256; i++)
+                    {
+                        seriesB.Points.AddXY(i, histogramB[i]);
+                    }
+
+                    chart2.Series.Add(seriesB);
+                    chart2.Visible = true;
+
+                    break;
+
+                case "Binary Formata Çevir":
+
+                    try
+                    {
+                        Bitmap image = new Bitmap(pictureBox1.Image);
+                        Bitmap binary = binaryYap(image);
+                        pictureBox2.Image = binary;
+                        pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Fotoğraf Yükleyiniz!");
+                    }
+                    break;
+
+                case "Görüntünün Sobel Kenar Bulma":
+
+                    Bitmap image2 = new Bitmap(pictureBox1.Image);
+                    Bitmap sobel = sobelYap(image2);
+                    pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+                    pictureBox2.Image = sobel;
+
+                    break;
+
+                default:
+                    break;
+            }
+        }
+            private Bitmap griYap(Bitmap bmp)
+            {
+                for (int i = 0; i < bmp.Height - 1; i++)
+                {
+                    for (int j = 0; j < bmp.Width - 1; j++)
+                    {
+                        int deger = (bmp.GetPixel(j, i).R + bmp.GetPixel(j, i).G + bmp.GetPixel(j, i).B) / 3;
+                        Color renk = Color.FromArgb(deger, deger, deger);
+
+                        bmp.SetPixel(j, i, renk);
+                    }
+                }
+                return bmp;
+            }
+
+            private Bitmap binaryYap(Bitmap bmp)
+            {
+                int tmp = 0;
+                Bitmap gri = griYap(bmp);
+                int esik = esikBul(gri);
+                Color renk;
+                for (int i = 0; i < gri.Height - 1; i++)
+                {
+                    for (int j = 0; j < gri.Width - 1; j++)
+                    {
+                        tmp = gri.GetPixel(j, i).G;
+                        if (tmp < esik)
+                        {
+                            renk = Color.FromArgb(0, 0, 0);
+                            gri.SetPixel(j, i, renk);
+                        }
+                        else
+                        {
+                            renk = Color.FromArgb(255, 255, 255);
+                            gri.SetPixel(j, i, renk);
+                        }
+                    }
+
+                }
+                return gri;
+            }
+
+            private Bitmap sobelYap(Bitmap image)
+            {
+                Bitmap gri = griYap(image);
+                Bitmap buffer = new Bitmap(gri.Width, gri.Height); //görüntünün boyutlarına sahip boş görüntü oluşturuyorsun
+                Color renk;
+                int valx, valy, gradient;
+                int[,] GX = new int[3, 3];
+                int[,] GY = new int[3, 3];
+
+                //Yatay kenar 
+                GX[0, 0] = -1; GX[0, 1] = 0; GX[0, 2] = 1;
+                GX[1, 0] = -2; GX[1, 1] = 0; GX[1, 2] = 2;
+                GX[2, 0] = -1; GX[2, 1] = 0; GX[2, 2] = 1;
+
+                //Dikey kenar
+                GY[0, 0] = -1; GY[0, 1] = -2; GY[0, 2] = -1;
+                GY[1, 0] = 0; GY[1, 1] = 0; GY[1, 2] = 0;
+                GY[2, 0] = 1; GY[2, 1] = 2; GY[2, 2] = 1;
+
+
+                for (int i = 0; i < image.Height; i++)
+                {
+                    for (int j = 0; j < image.Width; j++)
+                    {
+                        if (i == 0 || i == gri.Height - 1 || j == 0 || j == gri.Width - 1)
+                        {
+                            renk = Color.FromArgb(255, 255, 255);
+                            buffer.SetPixel(j, i, renk);
+                            valx = 0;
+                            valy = 0;
+                        }
+                        else
+                        {
+                            //bütün x koordinatları için bakılıyor
+                            valx = gri.GetPixel(j - 1, i - 1).R * GX[0, 0]
+                                + gri.GetPixel(j, i - 1).R * GX[0, 1]
+                                + gri.GetPixel(j + 1, i - 1).R * GX[0, 2]
+                                + gri.GetPixel(j - 1, i).R * GX[1, 0]
+                                + gri.GetPixel(j, i).R * GX[1, 1]
+                                + gri.GetPixel(j + 1, i).R * GX[1, 2]
+                                + gri.GetPixel(j - 1, i + 1).R * GX[2, 0]
+                                + gri.GetPixel(j, i + 1).R * GX[2, 1]
+                                + gri.GetPixel(j + 1, i + 1).R * GX[2, 2];
+
+                            //bütün y koordinatları için bakılıyor
+                            valy = gri.GetPixel(j - 1, i - 1).R * GY[0, 0]
+                                 + gri.GetPixel(j, i - 1).R * GY[0, 1]
+                                 + gri.GetPixel(j + 1, i - 1).R * GY[0, 2]
+                                 + gri.GetPixel(j - 1, i).R * GY[1, 0]
+                                 + gri.GetPixel(j, i).R * GY[1, 1]
+                                 + gri.GetPixel(j + 1, i).R * GY[1, 2]
+                                 + gri.GetPixel(j - 1, i + 1).R * GY[2, 0]
+                                 + gri.GetPixel(j, i + 1).R * GY[2, 1]
+                                 + gri.GetPixel(j + 1, i + 1).R * GY[2, 2];
+
+                            gradient = (int)(Math.Abs(valx) + Math.Abs(valy));
+
+                            if (gradient < 0)
+                                gradient = 0;
+                            if (gradient > 255)
+                                gradient = 255;
+
+                            renk = Color.FromArgb(gradient, gradient, gradient);
+                            buffer.SetPixel(j, i, renk);
+                        }
+                    }
+                }
+                return buffer; ;
+            }
+
+            int esikBul(Bitmap gri)
+            {
+                int enb = gri.GetPixel(0, 0).G;
+                int enk = gri.GetPixel(0, 0).G;
+                for (int i = 0; i < gri.Height - 1; i++)
+                {
+                    for (int j = 0; j < gri.Width - 1; j++)
+                    {
+                        if (enb > gri.GetPixel(j, i).G)
+                            enb = gri.GetPixel(j, i).G;
+                        if (enk < gri.GetPixel(j, i).G)
+                            enk = gri.GetPixel(j, i).G;
+                    }
+                }
+                int a = enb;
+                int b = enk;
+                esik = (a + b) / 2;
+                return esik;
+            }
+
+            private Bitmap KMeansSegmentasyon(Image image, int deger)
+            {
+                Bitmap bmp = new Bitmap(image);
+                Random random = new Random();
+
+                int k = deger;
+                int iterasyonSayisi = 10;
+
+                List<Color> merkezRenkler = new List<Color>(); // rastgele başlangıç merkezleri oluştur
+                for (int i = 0; i < k; i++)
+                {
+                    int x = random.Next(0, bmp.Width);
+                    int y = random.Next(0, bmp.Height);
+                    merkezRenkler.Add(bmp.GetPixel(x, y));
+                }
+
+                for (int iterasyon = 0; iterasyon < iterasyonSayisi; iterasyon++)
+                {
+                    List<List<Color>> gruplar = new List<List<Color>>(); // her pikseli en yakın merkeze atayarak grupla
+                    for (int i = 0; i < k; i++)
+                    {
+                        gruplar.Add(new List<Color>());
+                    }
+
+                    for (int y = 0; y < bmp.Height; y++)
+                    {
+                        for (int x = 0; x < bmp.Width; x++)
+                        {
+                            Color pixelRenk = bmp.GetPixel(x, y);
+
+                            // En yakın merkezi bul
+                            int enYakinDeger = 0;
+                            double enKMesafe = double.MaxValue;
+                            for (int i = 0; i < k; i++)
+                            {
+                                double mesafe = RenkMesafe(pixelRenk, merkezRenkler[i]);
+                                if (mesafe < enKMesafe)
+                                {
+                                    enYakinDeger = i;
+                                    enKMesafe = mesafe;
+                                }
+                            }
+
+                            gruplar[enYakinDeger].Add(pixelRenk);
+                        }
+                    }
+
+                    for (int i = 0; i < k; i++)
+                    {
+                        if (gruplar[i].Count > 0)
+                        {
+                            Color yeniMerkezDegeri = GrupOrtalamaRenk(gruplar[i]); // gruplara yeni merkezler atar
+                            merkezRenkler[i] = yeniMerkezDegeri;
+                        }
+                    }
+                }
+
+                Bitmap segmentasyonSonuc = new Bitmap(bmp.Width, bmp.Height); // yeni bir bitmap oluştur ve pikselleri gruplara göre renklendir
+                for (int y = 0; y < bmp.Height; y++)
+                {
+                    for (int x = 0; x < bmp.Width; x++)
+                    {
+                        Color pixelRenk = bmp.GetPixel(x, y);
+
+                        int enYakinDeger = 0;
+                        double enKucukMesafe = double.MaxValue;
+                        for (int i = 0; i < k; i++)
+                        {
+                            double mesafe = RenkMesafe(pixelRenk, merkezRenkler[i]);
+                            if (mesafe < enKucukMesafe)
+                            {
+                                enYakinDeger = i;
+                                enKucukMesafe = mesafe;
+                            }
+                        }
+                        segmentasyonSonuc.SetPixel(x, y, merkezRenkler[enYakinDeger]); // pikseli en yakın merkezin rengine ayarla
+                    }
+                }
+
+                return segmentasyonSonuc;
+            }
+            private double RenkMesafe(Color renk1, Color renk2)
+            {
+                double rMesafe = Math.Pow(renk1.R - renk2.R, 2); // red için aralarındaki mesafeyi hesaplar
+                double gMesafe = Math.Pow(renk1.G - renk2.G, 2); // green için aralarındaki mesafeyi hesaplar
+                double bMesafe = Math.Pow(renk1.B - renk2.B, 2); // blue için aralarındaki mesafeyi hesaplar
+
+                return Math.Sqrt(rMesafe + gMesafe + bMesafe);
+            }
+            private Color GrupOrtalamaRenk(List<Color> grup)
+            {
+                int toplamR = 0;
+                int toplamG = 0;
+                int toplamB = 0;
+
+                foreach (Color renk in grup)
+                {
+                    toplamR += renk.R;
+                    toplamG += renk.G;
+                    toplamB += renk.B;
+                }
+
+                int ortalamaR = toplamR / grup.Count;
+                int ortalamaG = toplamG / grup.Count;
+                int ortalamaB = toplamB / grup.Count;
+
+                return Color.FromArgb(ortalamaR, ortalamaG, ortalamaB);
+            }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            chart1.Visible = false;
+            chart2.Visible = false;
+
+            for (int i = 1; i < 10; i++)
+            {
+                comboBox2.Items.Add(i);
+            }
+        }
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox2.SelectedItem != null)
+            {
+                secilenCombobox = Convert.ToInt32(comboBox2.SelectedItem);
+
+            }
+        }
+    }
+}
